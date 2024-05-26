131274d8a700abd6a029d2b0af93182737956737
Om Singh <ninjaplays792020@gmail.com>
2024-05-26 12:06:35 +0530

    made changes to program 6 of assignment 3.

--- Lab Assignment 3/P6.java
+++ Lab Assignment 3/P6.java
@@ -4,15 +4,18 @@ public class P6 {
     public static <T> int count(T array[], T item) {
         int c = 0;
         for (int i = 0; i < array.length; i++) {
-            if(array[i]==item)
+             
+            if(array[i].equals(item))
                 c++;
         }
         return c;
     }
     public static void main(String args[]) {
-        Integer intArray[] = { 1,2,3,1,3,1,4,1};
+        Integer intArray[] = { 1, 2, 3, 1, 3, 1, 4, 1 };
+        Double doubleArray[] = { 3.14, 4.56, 3.14, 7.84, 3.14 };
         Character charArray[] = { 'A', 'B', 'A', 'D', 'A' };
         System.out.println("count of the int item is: " + count(intArray, 1));
+        System.out.println("count of double item is: " + count(doubleArray, 3.14));
         System.out.println("count of the char item is: " + count(charArray, 'A'));
     }
 }
